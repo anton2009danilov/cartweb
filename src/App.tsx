1f1f794c1f3775a5c46f4db8490880bb18c70094
anton2009danilov <anton2009danilov@yandex.ru>
2021-11-23 17:10:52 +0300

    small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import FieldWithMoveablePoint from './components/FieldWithMoveablePoint';
+import FieldWithPointsGeneration from './components/FieldWithPointsGeneration';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <FieldWithMoveablePoint/>
+      <FieldWithPointsGeneration/>
     </div>
   );
 }
--- src/components/FieldWithMoveablePoint.tsx
+++ src/components/FieldWithMoveablePoint.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import styles from "./FieldWithMoveablePoint.module.css";
+import styles from "./Fields.module.css";
 
 const FieldWithMoveablePoint: React.FC  = () => {
   const [circleX, setCircleX] = useState(30);
   const [circleY, setCircleY] = useState(30);
-  const onClickRect = (e: React.MouseEvent<SVGRectElement, MouseEvent> ) => {
+  const onClickRect = (e: React.MouseEvent<HTMLDivElement, MouseEvent> ) => {
     e.preventDefault();
-    setCircleX(e.nativeEvent.offsetX);
-    setCircleY(e.nativeEvent.offsetY);
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    setCircleX(x);
+    setCircleY(y);
   };
 
   return (
-    <div className={styles.container}>
-        <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-        <rect x="0" y="0" width="300" height="300" fill="#c0c0fa" onClick={ (e) => onClickRect(e) }/>
+    <div className={styles.container} onClick={ (e) => onClickRect(e) }>
+      <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
         <circle id="point" cx={ circleX } cy={ circleY } r="3" fill="#ff0000"/>
       </svg>
   </div>
