98277667ffbaaa0735d561da090bd32aa3ec1c30
anton2009danilov <anton2009danilov@yandex.ru>
2021-11-23 16:06:33 +0300

    small fixes for FieldWithMoveablePoint module

--- src/components/FieldWithMoveablePoint.tsx
+++ src/components/FieldWithMoveablePoint.tsx
@@ -6,24 +6,17 @@ const FieldWithMoveablePoint: React.FC  = () => {
   const [circleY, setCircleY] = useState(30);
   const onClickRect = (e: React.MouseEvent<SVGRectElement, MouseEvent> ) => {
     e.preventDefault();
-    // setCircleX(e.pageX);
-    // setCircleY(e.pageY);
     setCircleX(e.nativeEvent.offsetX);
     setCircleY(e.nativeEvent.offsetY);
-    console.log(e.nativeEvent.pageX);
-    console.log(e.nativeEvent.offsetX);
-    console.log(e.nativeEvent.pageY);
-    console.log(e.nativeEvent.offsetY);
-    // console.log(e);
   };
 
   return (
     <div className={styles.container}>
-      <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
         <rect x="0" y="0" width="300" height="300" fill="#c0c0fa" onClick={ (e) => onClickRect(e) }/>
         <circle id="point" cx={ circleX } cy={ circleY } r="3" fill="#ff0000"/>
       </svg>
-    </div>
+  </div>
   )
 };
 
