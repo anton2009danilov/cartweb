6aedb0338e2eb63b74f07edf3cf359e99d74a918
anton2009danilov <anton2009danilov@yandex.ru>
2021-11-24 13:41:58 +0300

    clean code of FieldWithMoveablePoint module

--- src/components/Fields/FieldWithMoveablePoint.tsx
+++ src/components/Fields/FieldWithMoveablePoint.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Fields.module.css";
 
 interface IFieldWithMoveablePointState {
@@ -34,26 +34,3 @@ export class FieldWithMoveablePoint extends React.Component<{}, IFieldWithMoveab
     );
   }
 }
-
-// const FieldWithMoveablePoint: React.FC  = () => {
-//   const [circleX, setCircleX] = useState(30);
-//   const [circleY, setCircleY] = useState(30);
-//   const onClickRect = (e: React.MouseEvent<HTMLDivElement, MouseEvent> ) => {
-//     e.preventDefault();
-//     const x = e.nativeEvent.offsetX;
-//     const y = e.nativeEvent.offsetY;
-//     setCircleX(x);
-//     setCircleY(y);
-//   };
-
-//   return (
-//     <div className={styles.container} onClick={ (e) => onClickRect(e) }>
-//       <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-//         <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
-//         <circle id="point" cx={ circleX } cy={ circleY } r="3" fill="#ff0000"/>
-//       </svg>
-//     </div>
-//   )
-// };
-
-// export default FieldWithMoveablePoint;
