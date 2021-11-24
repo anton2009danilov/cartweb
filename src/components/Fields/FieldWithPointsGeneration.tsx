90c88ccc5b93d68c7ea5bd07b355161884e7d8ce
Anton Danilov <anton2009danilov@yandex.ru>
2021-11-24 10:19:44 +0300

    files reorganization, small fixes

--- src/components/Fields/FieldWithPointsGeneration.tsx
+++ src/components/Fields/FieldWithPointsGeneration.tsx
@@ -11,14 +11,15 @@ const FieldWithPointsGeneration: React.FC  = () => {
     setPointsList({ points });
   };
 
+const pointElements = pointsList.points.map((el) => (
+  <circle id="point" cx={ el.x } cy={ el.y } r="3" fill="#ff0000"/>
+));
+
   return (
     <div className={styles.container} onClick={ (e) => onClickRect(e) }>
       <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
         <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
-        { pointsList.points.map( (el) => (
-            <circle id="point" cx={ el.x } cy={ el.y } r="3" fill="#ff0000"/>
-          )
-        )}
+        { pointElements }
       </svg>
     </div>
   )
