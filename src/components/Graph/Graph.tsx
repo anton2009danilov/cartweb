2d8325b3d4a0467623c5faa0aabbb63499fe3588
anton2009danilov <anton2009danilov@yandex.ru>
2021-11-24 16:30:44 +0300

    update Graph module: add MouseMove event

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -5,13 +5,17 @@ interface IGraphProps {
   values: Array<number>,
 }
 
-export class Graph extends React.Component<IGraphProps> {
-  valueElements: Array<any>;
+interface IGraphState {
+  dashLineX: number,
+  valueElements: Array<object>
+}
 
+export class Graph extends React.Component<IGraphProps, IGraphState> {
   constructor(props: IGraphProps) {
     super(props);
     const valuesList = this.calcValuesParams(props.values).valuesList;
-    this.valueElements = valuesList.map((el) => (
+    
+    const valueElements = valuesList.map((el) => (
       <rect 
         rx="5" ry="5"
         x={ el.x} y={ el.y }
@@ -19,33 +23,52 @@ export class Graph extends React.Component<IGraphProps> {
         fill="url('#myGradient')"
       >
         <animate attributeName="height" from="0" to={ el.height } dur="0.5s" fill="freeze" />
-      </rect>));
+      </rect>)
+    );
+
+    this.state = {
+      dashLineX: 0,
+      valueElements,
+    }
   };
 
   calcValuesParams(valuesList: Array<number>){
     return valuesList.reduce(
-      (acc: {valuesList: Array<{ x: number, y: number, width: number, height: number }>, counter: number}, val) => {
+      (acc: {
+        valuesList: Array<{ x: number, y: number, width: number, height: number }>,
+        counter: number
+        }, val) => {
         const height = val * 20 + 5;
         const width = 20;
         const x = acc.counter ? (acc.counter * 30 + 10) : 10;
         const y = -5;
         const counter = acc.counter + 1;
         return { valuesList: [...acc.valuesList, { x, y, width, height}], counter };
-      }, { valuesList: [], counter: 0 });
-    }
+    }, { valuesList: [], counter: 0 });
+  }
+
+  handleMouseMove(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.preventDefault();
+    const x = e.nativeEvent.offsetX;
+    this.setState({ dashLineX: x });
+    console.log(this);
+  }
 
   render() {
     return  (
-      <div className={styles.container}>
+      <div className={styles.container} onMouseMove={ (e) => this.handleMouseMove(e)}>
         <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg" transform="scale(1, -1)">
           <defs>
             <linearGradient id="myGradient" gradientTransform="rotate(90)">
-              <stop offset="5%"  stop-color="gold" />
-              <stop offset="95%" stop-color="red" />
+              <stop offset="5%"  stopColor="gold" />
+              <stop offset="95%" stopColor="red" />
             </linearGradient>
           </defs>
           <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
-          { this.valueElements }
+
+          { this.state.valueElements }
+
+          <line x1={ this.state.dashLineX } y1="0" x2={ this.state.dashLineX } y2="300" stroke="rgba(149, 165, 166, 1)" stroke-dasharray="3"/>
         </svg>
       </div>
     )
