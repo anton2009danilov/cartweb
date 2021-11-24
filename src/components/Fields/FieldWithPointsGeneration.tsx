0dbcc1e85faad2191f05848b52ea9cae85304632
anton2009danilov <anton2009danilov@yandex.ru>
2021-11-24 14:29:23 +0300

    change import links in App.tsx

--- src/components/Fields/FieldWithPointsGeneration.tsx
+++ src/components/Fields/FieldWithPointsGeneration.tsx
@@ -1,28 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Fields.module.css";
 
-const FieldWithPointsGeneration: React.FC  = () => {
-  const [pointsList, setPointsList] = useState({ points: [{ x: -10, y: -10 }] });
-  const onClickRect = (e: React.MouseEvent<HTMLDivElement, MouseEvent> ) => {
+interface IFieldWithPointsGenerationState {
+  pointsList: Array<any>,
+  pointElements: Object,
+  nextPointElementKey: number,
+}
+
+export class FieldWithPointsGeneration extends React.Component<any, IFieldWithPointsGenerationState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      pointsList: [],
+      pointElements: [],
+      nextPointElementKey: 0,
+    }
+  }
+
+  onClickRect(e: React.MouseEvent<HTMLDivElement, MouseEvent> ) {
     e.preventDefault();
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
-    const points = [...pointsList.points, { x, y }];
-    setPointsList({ points });
+    const key = this.state.nextPointElementKey;
+    const pointsList = [...this.state.pointsList, { x, y }];
+    const pointElements = pointsList.map((el: { x: number, y: number, key: number}) => (
+      <circle id="point" key={ el.key} cx={ el.x } cy={ el.y } r="3" fill="#ff0000"/>
+    ));
+
+    this.setState({
+      pointsList,
+      pointElements,
+      nextPointElementKey: key + 1,
+    });
   };
-
-  const pointElements = pointsList.points.map((el) => (
-    <circle id="point" cx={ el.x } cy={ el.y } r="3" fill="#ff0000"/>
-  ));
-
-  return (
-    <div className={styles.container} onClick={ (e) => onClickRect(e) }>
-      <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-        <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
-        { pointElements }
-      </svg>
-    </div>
-  )
-};
-
-export default FieldWithPointsGeneration;
+  
+  render() {
+    return (
+      <div className={styles.container} onClick={ (e) => this.onClickRect(e) }>
+        <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+          <rect x="0" y="0" width="300" height="300" fill="#c0c0fa"/>
+          { this.state.pointElements }
+        </svg>
+      </div>
+    );
+  }
+}
